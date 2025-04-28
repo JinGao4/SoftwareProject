c85f7dfaac668904c3b74c16b8880c56dbaf60ba
JinGao4 <157361689+JinGao4@users.noreply.github.com>
2025-04-28 09:38:09 +0100

    had a problem with route , now fixed

--- project/routes/web.php
+++ project/routes/web.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\DonationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
